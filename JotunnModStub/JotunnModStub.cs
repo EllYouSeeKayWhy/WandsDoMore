24ce2d9d2cabea4c3b6a7eba0698ff6ec09b8bab
EllYouSeeKayWhy <EllYouSeeKay@gmail.com>
2023-01-09 18:55:15 -0800

    Add convocate to Dead Raiser with config option for key

--- JotunnModStub/JotunnModStub.cs
+++ JotunnModStub/JotunnModStub.cs
@@ -1,35 +1,87 @@
-﻿// JotunnModStub
-// a Valheim mod skeleton using Jötunn
+﻿// WandsDoMore
+// a Valheim mod to add functionality to vanilla magic
 // 
-// File:    JotunnModStub.cs
-// Project: JotunnModStub
+// File:    WandsDoMore.cs
+// Project: WandsDoMore
 
 using BepInEx;
-using Jotunn.Entities;
+using BepInEx.Configuration;
+using Jotunn.Configs;
 using Jotunn.Managers;
+using System.Collections.Generic;
+using UnityEngine;
 
-namespace JotunnModStub
+namespace WandsDoMore
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
     //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
-    internal class JotunnModStub : BaseUnityPlugin
+    internal class WandsDoMore : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.jotunn.jotunnmodstub";
-        public const string PluginName = "JotunnModStub";
-        public const string PluginVersion = "0.0.1";
-        
-        // Use this class to add your own localization to the game
-        // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
-        public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
+        public const string PluginGUID = "com.ellyouseekaywhy.WandsDoMore";
+        public const string PluginName = "WandsDoMore";
+        public const string PluginVersion = "0.0.2";
+
+        public ButtonConfig ConvocateButton;
+
+        private ConfigEntry<KeyCode> ConvocateConfig;
 
         private void Awake()
         {
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
-            Jotunn.Logger.LogInfo("ModStub has landed");
-            
-            // To learn more about Jotunn's features, go to
-            // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
+            Jotunn.Logger.LogInfo("WandsDoMore has landed");
+            CreateConfigValues();
+            AddInputs();
+        }
+
+        private void Update()
+        {
+            if (ZInput.instance != null)
+            {
+                // Check if hotkey is used while holding correct magic item
+                if (ConvocateButton != null && ZInput.GetButtonDown(ConvocateButton.Name) && Player.m_localPlayer != null && Player.m_localPlayer.m_visEquipment.m_leftItem == "StaffSkeleton")
+                {
+                    TeleportFollowingMinionsToPlayer(Player.m_localPlayer);
+                }
+            }
+        }
+
+        private void CreateConfigValues()
+        {
+            Config.SaveOnConfigSet = true;
+            ConvocateConfig = Config.Bind("Client config", "Convocate Activation", KeyCode.G,
+                new ConfigDescription("Key to Convocate recalling following minions to you"));
+        }
+
+        private void AddInputs()
+        {
+            ConvocateButton = new ButtonConfig
+            {
+                Name = "Convocate",
+                Config = ConvocateConfig,
+
+                BlockOtherInputs = true
+            };
+            InputManager.Instance.AddButton(PluginGUID, ConvocateButton);
+        }
+
+        public void TeleportFollowingMinionsToPlayer(Player player)
+        {
+            // based off BaseAI.FindClosestCreature
+            List<Character> allCharacters = Character.GetAllCharacters();
+            foreach (Character item in allCharacters)
+            {
+                if (item.IsDead())
+                {
+                    continue;
+                }
+
+                if (item.GetComponent<MonsterAI>()?.GetFollowTarget() == player.gameObject)
+                {
+                    item.transform.position = player.transform.position;
+                }
+            }
         }
     }
 }
+
